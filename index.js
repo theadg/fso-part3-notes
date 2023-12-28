069336d0a1a825a512a06fb794f86b952af29994
Bernard De Guzman <bernard.deguzman@technomancer.biz>
2023-12-28 20:20:52 +0800

    Add frontend and connect to mongo

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+const Note = require('./models/note')
+
 const app = express()
 
 /**
@@ -17,37 +20,62 @@ app.use(express.json())
  */
 app.use(cors())
 
+/**
+ * @Middleware
+ * -> makes express show static content
+ * -> "static content" = dist folder (contains minified files)
+ */
 app.use(express.static('dist'))
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {   
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },  
-    {   
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+// let notes = [
+//     {
+//         id: 1,
+//         content: "HTML is easy",
+//         important: true
+//     },
+//     {   
+//         id: 2,
+//         content: "Browser can execute only JavaScript",
+//         important: false
+//     },  
+//     {   
+//         id: 3,
+//         content: "GET and POST are the most important methods of HTTP protocol",
+//         important: true
+//     }
+// ]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello world from nodemon</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.post('/api/notes', async (request, response) => {
+    const { body } = request
+    const { content, important } = body
+
+    if (content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+
+    const note = await Note.create({
+        content,
+        important: important || false,
+    })
+
+    console.log(note)
+    return response.json(note);
+});
+
+app.get('/api/notes', async (request, response) => {
+    const notes = await Note.find();
+
+    // * Return as JSON
+    return response.json(notes);
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+app.get('/api/notes/:id', async (request, response) => {
+    const id = request.params.id
+    const note = await Note.findById(id);
 
     if (!note) {
         return response.status(404).send(`Note with ${id} not found`)
