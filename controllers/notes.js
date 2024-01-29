3ad15d9e81cc74d5b9897929b06cd65249068a83
Bernard De Guzman <bernard.deguzman@technomancer.biz>
2024-01-29 20:46:09 +0800

    feature: add token based auth

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,11 +1,21 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        console.log(authorization)
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 notesRouter.get('/', async (request, response) => {
     const notes = await Note.find({}).populate('user', {
         username: 1,
-        name: 1
+        name: 1,
     })
 
     response.json(notes)
@@ -22,8 +32,13 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
-    const user = await User.findById(body.userId)
+    // const user = await User.findById(body.userId)
 
     const note = new Note({
         content: body.content,
